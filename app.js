4fe46288d90ed6afee76ddbc403e1ea8ef13147e
natesan.sivagnanam <natesan.sivagnanam@sap.com>
2018-09-13 09:09:23 +0530

    Introduced operation modes & npm dependencies

--- app.js
+++ app.js
@@ -14,9 +14,12 @@ var aRemovedFileList = [];
 var aSourceFileList = [];
 var aTargetFileList = [];
 var aDiffContent = [];
+var sMode = '';
 
 var constants = {
-  CHECK_DIRECTORY: "Check Directories"
+  COMPARE_DIRECTORIES_ONLY: "Compare Directories Only",
+  COMPARE_DIRECTORIES_REPORT: "Compare Directories and Report",
+  COMPARE_DIRECTORIES_REPORT_DIFF: "Compare Directories, Report differences & Create Patch File"
 };
 
 var sourceSettings = {
@@ -36,36 +39,38 @@ inquirer.prompt([{
   name: 'action',
   message: 'What do you want to perform?',
   choices: [
-    constants.CHECK_DIRECTORY
+    constants.COMPARE_DIRECTORIES_ONLY,
+    constants.COMPARE_DIRECTORIES_REPORT,
+    constants.COMPARE_DIRECTORIES_REPORT_DIFF
   ]
 }]).then(answers => {
   initialize(answers);
 });
 
 var initialize = function (answers) {
-  if (answers.action === constants.CHECK_DIRECTORY) {
-    fnAsyncDirectoryRead(sourceSettings).then(function (result) {
-      //console.log("\nPromise Returned : Source List:")
-      aSourceFileList = result;
-      //console.log(aSourceFileList);
-      fnAsyncDirectoryRead(targetSettings).then(function (result) {
-        //console.log("\nPromise Returned : Target List:")
-        aTargetFileList = result;
-        //console.log(aTargetFileList);
-        fnCompareFileList(aSourceFileList, aTargetFileList);
-        fnCompareFileContent();
-        fnWriteReport();
-      });
+  sMode = answers.action;
+
+  fnAsyncDirectoryRead(sourceSettings).then(function (result) {
+    aSourceFileList = result;
+    fnAsyncDirectoryRead(targetSettings).then(function (result) {
+      aTargetFileList = result;
+
+
+      fnCompareFileList(aSourceFileList, aTargetFileList);
+      fnCompareFileContent();
+
+      fnWriteReport();
     });
-  }
+  });
+
 }
 
-var fnCompareFileList = function (aSrcFileList, aDestFileList) {
-  if (!aSrcFileList || !aDestFileList)
+var fnCompareFileList = function (srcFileList, destFileList) {
+  if (!srcFileList || !destFileList)
     return;
 
-  aSrcFileList.forEach((srcElement) =>
-    aDestFileList.forEach((destElement) => {
+  srcFileList.forEach((srcElement) =>
+    destFileList.forEach((destElement) => {
       if (srcElement == destElement) {
         aFinalList.push(srcElement);
       }
@@ -74,44 +79,36 @@ var fnCompareFileList = function (aSrcFileList, aDestFileList) {
   console.log("\nFinal Matching List:" + aFinalList.length);
   console.log(aFinalList);
 
-  aAddedFileList = _.difference(aDestFileList, aSrcFileList);
+  aAddedFileList = _.difference(destFileList, srcFileList);
   console.log("\nAdded in Destination:" + aAddedFileList.length);
   console.log(aAddedFileList);
 
-  aRemovedFileList = _.difference(aSrcFileList, aDestFileList);
+  aRemovedFileList = _.difference(srcFileList, destFileList);
   console.log("\nDeleted from Source:" + aRemovedFileList.length);
   console.log(aRemovedFileList);
 }
 
 var fnCompareFileContent = function () {
   console.log("\nComparing File Content for Files available in both directories...");
-   
+
   aFinalList.forEach((aFinalListItem) => {
     var srcFileContent = fs.readFileSync(rootDir + '/src/' + aFinalListItem, "utf8");
     var destFileContent = fs.readFileSync(rootDir + '/dest/' + aFinalListItem, "utf8");
-    
+
     var diff = jsdiff.structuredPatch(rootDir + '/src/' + aFinalListItem, rootDir + '/dest/' + aFinalListItem,
-    srcFileContent, destFileContent,
-    '/src/' + aFinalListItem, '/dest/' + aFinalListItem);
+      srcFileContent, destFileContent,
+      '/src/' + aFinalListItem, '/dest/' + aFinalListItem);
     if (diff.hunks[0] && diff.hunks[0].lines) {
-      //console.log("\nDifference between '" + diff.oldHeader + "' and '" + diff.newHeader + "'");
-      //console.log(diff.hunks[0].lines);
       aDiffContent.push(diff);
-    }  
+    }
   });
 }
 
-var fnAsyncDirectoryRead = function (oSettings) {
+var fnAsyncDirectoryRead = function (settings) {
   return new Promise(function (resolve, reject) {
-    readdirp(oSettings)
+    readdirp(settings)
       .on('data', function (entry) {
-        //console.log(entry);
         aFileList.push(entry.path);
-
-        // aFileList.push({
-        //   fileName : entry.path,
-        //   fullPath : entry.fullPath
-        // });
       })
       .on('warn', function (warn) {
         console.log("Warn: ", warn);
@@ -127,39 +124,49 @@ var fnAsyncDirectoryRead = function (oSettings) {
   });
 }
 
-var fnWriteReport = function(){
+var fnWriteReport = function () {
+  var sReportContent = [];
   var sDiffContent = [];
-
-  console.log("\nWriting Report to File...");
+  
   console.log("\nFinal Matching List:" + aFinalList.length);
   console.log("\nAdded in Destination:" + aAddedFileList.length);
   console.log("\nDeleted from Source:" + aRemovedFileList.length);
 
-  sDiffContent.push("\nFinal Matching List:" + aFinalList.length);
-  sDiffContent.push("\nAdded in Destination:" + aAddedFileList.length);
-  sDiffContent.push("\nDeleted from Source:" + aRemovedFileList.length);
+  sReportContent.push("\nFinal Matching List:" + aFinalList.length);
+  sReportContent.push("\nAdded in Destination:" + aAddedFileList.length);
+  sReportContent.push("\nDeleted from Source:" + aRemovedFileList.length);
 
-  sDiffContent.push("\n\nFinal Matching List:\n");
+  sReportContent.push("\n\nFinal Matching List:\n");
   aFinalList.forEach((finalListItem) => {
-    sDiffContent.push(finalListItem + '\n');
-  });
-  
-  sDiffContent.push("\n\nAdded in Destination:\n");
-  aAddedFileList.forEach((addedListItem) => {
-    sDiffContent.push(addedListItem + '\n');
-  });
-  
-  sDiffContent.push("\n\nDeleted from Source:\n");
-  aRemovedFileList.forEach((removedListItem) => {
-    sDiffContent.push(removedListItem + '\n');
+    sReportContent.push(finalListItem + '\n');
   });
 
-  aDiffContent.forEach((diffItem) => {
-    sDiffContent.push("\n\nDifference between '" + diffItem.oldHeader + "' and '" + diffItem.newHeader + "'\n");
-    sDiffContent.push(diffItem.hunks[0].lines);
+  sReportContent.push("\n\nAdded in Destination:\n");
+  aAddedFileList.forEach((addedListItem) => {
+    sReportContent.push(addedListItem + '\n');
   });
-  
-  writeFile('result/diff.patch', sDiffContent, function(err) {
-    if (err) console.log(err);
+
+  sReportContent.push("\n\nDeleted from Source:\n");
+  aRemovedFileList.forEach((removedListItem) => {
+    sReportContent.push(removedListItem + '\n');
   });
+
+  if (sMode === constants.COMPARE_DIRECTORIES_REPORT || sMode === constants.COMPARE_DIRECTORIES_REPORT_DIFF) {
+    console.log("\nWriting Report to File...");
+    writeFile('result/result.txt', sReportContent, function (err) {
+      if (err) console.log(err);
+    });
+  }
+
+
+  if (sMode === constants.COMPARE_DIRECTORIES_REPORT_DIFF) {
+    aDiffContent.forEach((diffItem) => {
+      sDiffContent.push("\n\nDifference between '" + diffItem.oldHeader + "' and '" + diffItem.newHeader + "'\n");
+      sDiffContent.push(diffItem.hunks[0].lines);
+    });
+    console.log("\nCreating Patch File...");
+    writeFile('result/diff.patch', sDiffContent, function (err) {
+      if (err) console.log(err);
+    });
+  }
 }
